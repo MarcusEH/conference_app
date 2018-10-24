4e9b85732f09d4762d12bfbd5cdcd01015b25cde
Marcus Hamill <marcusehamill@gmail.com>
2018-10-24 12:54:03 -0500

    add loop to seeds file to fill in missing age and gender data

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,4 +1,9 @@
 class Speaker < ApplicationRecord
+
+  # validates :first_name, :last_name, length: { minimum: 2 }
+  # validates :email, uniqueness: true
+  # validates :age
+
   def friendly_updated_at
     updated_at.strftime("%A, %d %b %Y %l:%M %p")
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # Speaker.create({first_name: "Dani", last_name: "Zaghian", email: "danielle@actualize.co"})
 
-10.times do
-  speakerfaker = Speaker.new(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, email: "#{FFaker::Name.first_name}#{FFaker::Name.last_name}@gmail.com")
-  speakerfaker.save
-end
+# 10.times do
+#   speakerfaker = Speaker.new(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, email: "#{FFaker::Name.first_name}#{FFaker::Name.last_name}@gmail.com")
+#   speakerfaker.save
+# end
 
-meeting4 = Meeting.new({title: "Hop.js", agenda: "Hop.js: JavaScript Multitier Programming", time: "3:00 pm"})
-meeting4.save
+# meeting4 = Meeting.new({title: "Hop.js", agenda: "Hop.js: JavaScript Multitier Programming", time: "3:00 pm"})
+# meeting4.save
+
+speakers = Speaker.all
+
+speakers.each do |speaker|
+  speaker.gender = ["female", "male"].sample
+  speaker.age = rand(18..100)
+  speaker.save
+  speaker.errors.full_messages
+end
